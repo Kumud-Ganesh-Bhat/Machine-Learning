80e4b24e60896c3b98d186a261f2c51a13d205f7
Kumud Bhat <kumudganeshbhat@gmail.com>
2018-02-06 06:46:07 -0800

    Update costFunction.m

--- Logistic Regression/costFunction.m
+++ Logistic Regression/costFunction.m
@@ -7,28 +7,11 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Note: grad should have the same dimensions as theta
-%
-
 
 h_theta = sigmoid(X * theta); 
 J = (1/m) * (-y' * log(h_theta) - (1-y)' * log(1-h_theta));
 grad = (1/m) * X' * (h_theta-y);
-
-
-
-
-
-% =============================================================
 
 end
