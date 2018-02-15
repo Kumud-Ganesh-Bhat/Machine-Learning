446e3682e930366315b969f558a4ab12d2238e93
Kumud Bhat <kumudganeshbhat@gmail.com>
2018-02-14 16:25:50 -0800

    Update and rename ex3_nn.m to mainScript_nn.m

--- Neural Networks/mainScript_nn.m
+++ Neural Networks/mainScript_nn.m
@@ -1,11 +1,11 @@
 %% Initialization
 clear ; close all; clc
 
-%% Setup the parameters you will use for this exercise
+%% Setup the parameters we will use for this exercise
 input_layer_size  = 400;  % 20x20 Input Images of Digits
 hidden_layer_size = 25;   % 25 hidden units
 num_labels = 10;          % 10 labels, from 1 to 10   
-                          % (note that we have mapped "0" to label 10)
+                          % (note that "0" has been mapped to label 10)
 
 %% =========== Part 1: Loading and Visualizing Data =============
 % Load Training Data
@@ -24,8 +24,7 @@ fprintf('Program paused. Press enter to continue.\n');
 pause;
 
 %% ================ Part 2: Loading Pameters ================
-% In this part of the exercise, we load some pre-initialized 
-% neural network parameters.
+% loading some pre-initialized neural network parameters.
 
 fprintf('\nLoading Saved Neural Network Parameters ...\n')
 
@@ -33,10 +32,6 @@ fprintf('\nLoading Saved Neural Network Parameters ...\n')
 load('ex3weights.mat');
 
 %% ================= Part 3: Implement Predict =================
-%  After training the neural network, we would like to use it to predict
-%  the labels. You will now implement the "predict" function to use the
-%  neural network to predict the labels of the training set. This lets
-%  you compute the training set accuracy.
 
 pred = predict(Theta1, Theta2, X);
 
